b3e6d57816c9b0d135caca6389af60c73710b0bd
Travis Beatty <travisby@gmail.com>
2012-11-19 18:46:42 -0500

    Added first url!

--- static_website/views.py
+++ static_website/views.py
@@ -1,1 +1,9 @@
 # Create your views here.
+from django.shortcuts import render_to_response
+
+
+def index(request):
+    return render_to_response(
+        'index.html',
+        {'active': 'index'}
+    )
--- tricktech/urls.py
+++ tricktech/urls.py
@@ -1,17 +1,6 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import patterns, url
 
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'tricktech.views.home', name='home'),
-    # url(r'^tricktech/', include('tricktech.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', 'static_website.views.index', name='index'),
 )
