8d1c3bcaac98ae23a76f1b7b2561eb240eb79eeb
Travis Beatty <travisby@gmail.com>
2012-11-19 18:45:48 -0500

    Added static_website to isntalled apps

--- tricktech/settings.py
+++ tricktech/settings.py
@@ -124,10 +124,7 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
-    # Uncomment the next line to enable admin documentation:
-    # 'django.contrib.admindocs',
+    'static_website',
 )
 
 # A sample logging configuration. The only tangible logging
