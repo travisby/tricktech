ce25744e23bdd8ee1d784c6480462a7de8137ca1
Travis Beatty <travisby@gmail.com>
2012-11-19 18:05:20 -0500

    Updated static files

--- tricktech/settings.py
+++ tricktech/settings.py
@@ -1,7 +1,10 @@
 # Django settings for tricktech project.
+import os
+
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
+PATH =  os.path.dirname(os.path.realpath(__file__))
 
 ADMINS = (
     # ('Your Name', 'your_email@example.com'),
@@ -59,7 +62,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = '/tmp/'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -70,6 +73,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    os.path.join(PATH, 'static'),
 )
 
 # List of finder classes that know how to find static files in
