0eaa5b6be2642fd376e3ce221977f1ee2ed2890f
Travis Beatty <travisby@gmail.com>
2012-11-26 19:59:49 -0500

    Added admin and faq pages

--- tricktech/urls.py
+++ tricktech/urls.py
@@ -5,4 +5,6 @@ urlpatterns = patterns('',
     url(r'^$', 'static_website.views.index', name='index'),
     url(r'^contact/$', 'static_website.views.contact', name='contact'),
     url(r'^services/$', 'static_website.views.services', name='services'),
+    url(r'^admins/$', 'static_website.views.admin', name='admin'),
+    url(r'^faq/$', 'static_website.views.faq', name='faq'),
 )
