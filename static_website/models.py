a65a18c3518ce812602597525652f7432ecbf89b
Travis Beatty <travisby@gmail.com>
2012-11-26 19:56:40 -0500

    Added new models, and the unicode methods for them

--- static_website/models.py
+++ static_website/models.py
@@ -4,3 +4,23 @@ from django.db import models
 class Service(models.Model):
     name = models.CharField(max_length=30)
     price = models.DecimalField(max_digits=8, decimal_places=2)
+
+    def __unicode__(self):
+        return '%s -- $%sl' % (self.name, self.price)
+
+
+class CustomerService(models.Model):
+    person_first_name = models.CharField(max_length=30)
+    person_last_name = models.CharField(max_length=30)
+    service = models.ForeignKey(Service)
+    date = models.DateTimeField(auto_now=True)
+
+    def __unicode__(self):
+        return (
+            "%s %s bought %s at %s" %
+            (self.person_first_name,self.person_last_name, self.service, self.date)
+        )
+
+class Faq(models.Model):
+    question = models.TextField()
+    answer = models.TextField()
