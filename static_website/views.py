8f96b954c3517285b32067a76be579138674fee1
Travis Beatty <travisby@gmail.com>
2012-11-26 19:58:25 -0500

    Added some sweet new views -- what a bad commit message

--- static_website/views.py
+++ static_website/views.py
@@ -1,7 +1,9 @@
 # Create your views here.
 from django.shortcuts import render_to_response
-import static_website.models
+from django.http import HttpResponse
 
+import static_website.models
+import static_website.forms
 
 def index(request):
     return render_to_response(
@@ -11,11 +13,43 @@ def index(request):
 
 
 def contact(request):
+    if request.method == 'GET':
+        obj = {}
+        obj.update(
+            {
+                'active': 'contact',
+                'services': static_website.models.Service.objects.all(),
+                'form': static_website.forms.CustomerServiceModelForm,
+            }
+        )
+        return render_to_response(
+            'contact.html',
+            obj
+        )
+    elif request.method == 'POST':
+        form = static_website.forms.CustomerServiceModelForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return admin(request)
+        else:
+            return HttpResponse('itdonebroke')
+
+def admin(request):
     return render_to_response(
-        'contact.html',
-        {'active': 'contact'}
+        'admin.html',
+        {
+            'customer_services': static_website.models.CustomerService.objects.all()
+        }
+
     )
 
+def faq(request):
+    return render_to_response(
+        'faq.html',
+        {
+            'faqs': static_website.models.Faq.objects.all()
+        }
+    )
 
 def services(request):
     return render_to_response(
