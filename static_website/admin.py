2886beb6cb81844d79f77af6f265defd5299e933
Travis Beatty <travisby@gmail.com>
2012-12-03 17:59:25 -0500

    Added admin

---
+++ static_website/admin.py
@@ -0,0 +1,7 @@
+from static_website import models
+from django.contrib import admin
+
+admin.site.register(models.Faq)
+admin.site.register(models.Chat)
+admin.site.register(models.CustomerService)
+admin.site.register(models.Service)
