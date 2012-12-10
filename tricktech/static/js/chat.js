6cfe9538302f7d82ccab16cef466062c7f5ffa50
Travis Beatty <travisby@gmail.com>
2012-12-10 09:21:45 -0500

    Added a fadein to the new chat messages

--- tricktech/static/js/chat.js
+++ tricktech/static/js/chat.js
@@ -1,9 +1,25 @@
+// Constants
+FADEINSPEED = 1000;
+
 $chat = $('#chat');
 id = '';
 
+replaceChatWithoutMagic = function() {
+    $magic = $chat.find('#magic');
+    $chats = $magic.children();
+    $magic.remove();
+    $chats.appendTo($chat);
+};
 
 onSuccess = function(data) {
-    $(data).appendTo($chat);
+    // If it is blank, 1 is returned?
+    if (data.length <= 1) {
+        return;
+    }
+    // We add magic so we can keep track of our "latest" elements, and remove magic after fadein
+    $('<div id="magic" class="hide">' +data + '</div>').appendTo($chat);
+    $chat.find('#magic').fadeIn(FADEINSPEED, replaceChatWithoutMagic);
+    // update the latest ID for the next request
     id = $chat.find('.message').last().data('id');
 };
 
