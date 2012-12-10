96dcc705754ae32de24f0ddac3124f4a4eda986d
Travis Beatty <travisby@gmail.com>
2012-12-10 10:10:49 -0500

    fixed a stylistic error with our urlconf

--- tricktech/urls.py
+++ tricktech/urls.py
@@ -3,16 +3,17 @@ from django.contrib import admin
 
 admin.autodiscover()
 
-urlpatterns = patterns('',
-                       url(r'^$', 'static_website.views.index', name='index'),
-                       (r'^login/$', 'django.contrib.auth.views.login', {'template_name': 'login.html'}),
-                       url(r'^contact/$', 'static_website.views.contact', name='contact'),
-                       url(r'^services/$', 'static_website.views.services', name='services'),
-                       url(r'^admins/$', 'static_website.views.admin', name='admin'),
-                       url(r'^faq/$', 'static_website.views.faq', name='faq'),
-                       url(r'^chat/$', 'static_website.views.chat', name='chat'),
-                       url(r'^ajax_chat/(?P<last_message>.*)/$', 'static_website.views.ajax_chat', name='ajax_chat'),
-                       url(r'^ajax_chat/$', 'static_website.views.ajax_chat', name='ajax_chat_init'),
-                       url(r'^admin/', include(admin.site.urls)),
-                       url(r'^admin/doc/',include('django.contrib.admindocs.urls')),
+urlpatterns = patterns(
+    '',
+    url(r'^$', 'static_website.views.index', name='index'),
+    (r'^login/$', 'django.contrib.auth.views.login', {'template_name': 'login.html'}),
+    url(r'^contact/$', 'static_website.views.contact', name='contact'),
+    url(r'^services/$', 'static_website.views.services', name='services'),
+    url(r'^admins/$', 'static_website.views.admin', name='admin'),
+    url(r'^faq/$', 'static_website.views.faq', name='faq'),
+    url(r'^chat/$', 'static_website.views.chat', name='chat'),
+    url(r'^ajax_chat/(?P<last_message>.*)/$', 'static_website.views.ajax_chat', name='ajax_chat'),
+    url(r'^ajax_chat/$', 'static_website.views.ajax_chat', name='ajax_chat_init'),
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/doc/',include('django.contrib.admindocs.urls')),
 )
